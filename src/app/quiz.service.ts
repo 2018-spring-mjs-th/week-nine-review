8ea7f11e5c585c341dfc4bdb838ca08cd5ac15d8
tsteelematc <tsteele@madisoncollege.edu>
2018-04-05 19:36:14 -0500

    Added new quiz service method for slack-n-tell...

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -11,4 +11,13 @@ export class QuizService {
     return this.builtInAngularHttpService.get('https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Hard Quiz').toPromise();
 
   }
+
+  public saveQuiz(succeed: boolean): Promise<string> {
+     
+    return new Promise((resolve, reject) => {
+
+      // Run some code and either resolve it (success) or reject it (failure).
+      succeed ? resolve("Quiz Saved!") : reject("Save Failed!");
+    });
+  }
 }
