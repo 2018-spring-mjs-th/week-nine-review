a2d2a2d8a0987bf23daa2e501ae88fcc23db10a7
Aaron Meyer <armeyer2@madisoncollege.edu>
2018-04-05 19:37:11 -0500

    Button Handler texr added

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { reject } from 'q';
+import { resolve } from 'url';
 
 @Injectable()
 export class QuizService {
@@ -10,5 +12,13 @@ export class QuizService {
 
     return this.builtInAngularHttpService.get('https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Hard Quiz').toPromise();
 
+  }
+
+  public saveQuiz(succeed: boolean): Promise<string> {
+
+    return new Promise((resolve, reject) => {
+
+      succeed ? resolve("Quiz Saved") : reject("Error");
+    });
   }
 }
