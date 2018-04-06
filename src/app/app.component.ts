b4ec855e8117ca64876a22bd8488e0bc9103e18e
j-stoff <jacob.stoffregen@gmail.com>
2018-04-05 19:58:30 -0500

    Saving jstoffregen week 11 branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,7 +39,20 @@ export class AppComponent {
   quizzes: quizDisplay[] = [];
 
   ngOnInit() {
-    this.quizzes = this.quizSvc.getQuizzes();
+    //this.quizzes = this.quizSvc.getQuizzes();
+    let quizPromise = this.quizSvc.getQuizzes();
+
+    quizPromise
+        .then(
+            data => {
+                this.quizzes = JSON.parse(data.text());
+            }
+        )
+        .catch(
+            error => {
+                this.quizzes = [];
+            }
+        );
   }
 
   selectedQuiz: selectedQuizType = undefined;
@@ -52,5 +65,28 @@ export class AppComponent {
     let newQuiz = { name: "New Untitled Quiz", numberQuestions: 0};
     this.quizzes.push(newQuiz);
     this.selectedQuiz = newQuiz;
+  }
+
+  public updateQuiz() {
+      if (this.selectedQuiz != null) {
+        let promise: Promise<string>;
+        let passed = Math.floor(Math.random() * Math.floor(10));
+
+        promise = passed < 5 ? this.quizSvc.saveQuiz(true) : this.quizSvc.saveQuiz(false);
+
+        promise
+            .then(
+                text => {
+                    console.log(text);
+                }
+            )
+            .catch(
+                error => {
+                    console.log(error);
+                }
+            );
+        } else {
+            console.log("Select a quiz first...");
+        }
   }
 }
