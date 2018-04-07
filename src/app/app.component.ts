fcf3e50ed206127b28b1a8df779a3db78fcdfa05
bciancio <Brandonciancio@gmail.com>
2018-04-07 13:15:24 -0500

    spacing + save functionality
    Modify spacing.
    
    Save new quiz method now uses quiz service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,66 +2,76 @@ import { Component } from '@angular/core';
 import { QuizService } from './quiz.service';
 
 interface quizDisplay {
-  name: string;
-  numberQuestions: number;
+	name: string;
+	numberQuestions: number;
 }
 
 type selectedQuizType = quizDisplay | undefined;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'
-    , '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-  ]
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'
+    	, '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+	]
 })
 export class AppComponent {
 
-  title = 'week-nine-review';
+	title = 'week-nine-review';
 
-  color = "silver";
+	color = "silver";
 
-  isDangerous = true;
+	isDangerous = true;
 
-  public toggleDanger() {
-    this.isDangerous = !this.isDangerous;
-  }
+	public toggleDanger() {
+		this.isDangerous = !this.isDangerous;
+	}
 
-  stupid = "Stupid";
+	stupid = "Stupid";
 
-  //quizService: QuizService;
+	//quizService: QuizService;
 
-  // TS automatic properties!!!
-  constructor(private quizSvc: QuizService) {
-    //this.quizService = quizSvc;
-  }
+	// TS automatic properties!!!
+	constructor(private quizSvc: QuizService) {
+		//this.quizService = quizSvc;
+	}
 
-  quizzes: quizDisplay[] = [];
+	quizzes: quizDisplay[] = [];
 
-  ngOnInit() {
-    
-    //this.quizzes = this.quizSvc.getQuizzes();
+	ngOnInit() {
 
+		//this.quizzes = this.quizSvc.getQuizzes();
+		// This is how to consume (or use) a Promise.
+		this.quizSvc.getQuizzes()
+			.then(data => {
+				this.quizzes = data.json();
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
 
-    // This is how to consume (or use) a Promise.
-    this.quizSvc.getQuizzes()
-      .then(data => {
-        this.quizzes = data.json();
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+	selectedQuiz: selectedQuizType = undefined;
 
-  selectedQuiz: selectedQuizType = undefined;
+	makeQuizSelected(q: quizDisplay) {
+		this.selectedQuiz = q;
+	}
 
-  makeQuizSelected(q: quizDisplay) {
-    this.selectedQuiz = q;
-  }
+	addQuiz() {
+		let newQuiz = {
+			name: "New Untitled Quiz",
+			numberQuestions: 0
+		};
+		this.quizzes.push(newQuiz);
+		this.selectedQuiz = newQuiz;
 
-  addQuiz() {
-    let newQuiz = { name: "New Untitled Quiz", numberQuestions: 0};
-    this.quizzes.push(newQuiz);
-    this.selectedQuiz = newQuiz;
-  }
+		this.quizSvc.saveQuiz(true)
+			.then(data => {
+				let result = data;
+				console.log(result);
+			})
+			.catch(error => {
+
+			});
+	}
 }
