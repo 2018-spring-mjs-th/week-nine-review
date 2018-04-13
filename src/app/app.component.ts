cdfc65d4f3bc1cd280a64157e79e3f7bf722fae2
emilysha <ewsha.09@gmail.com>
2018-04-12 19:22:30 -0500

    Not getting add new question to work.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { QuizService } from './quiz.service';
 interface quizDisplay {
   name: string;
   numberQuestions: number;
+  questions: {name : string}[];
 }
 
 type selectedQuizType = quizDisplay | undefined;
@@ -37,20 +38,27 @@ export class AppComponent {
   }
 
   quizzes: quizDisplay[] = [];
+  questions: {name : string}[];
+
 
   ngOnInit() {
     
     //this.quizzes = this.quizSvc.getQuizzes();
 
-
+    console.log("Before Promise");
     // This is how to consume (or use) a Promise.
+    this.loadQuizzes();
+
+  }
+
+  private loadQuizzes() {
     this.quizSvc.getQuizzes()
-      .then(data => {
-        this.quizzes = data.json();
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    .then(data => {
+      this.quizzes = data.json();
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }
 
   selectedQuiz: selectedQuizType = undefined;
@@ -60,8 +68,35 @@ export class AppComponent {
   }
 
   addQuiz() {
-    let newQuiz = { name: "New Untitled Quiz", numberQuestions: 0};
+    let newQuiz = { name: "New Untitled Quiz", numberQuestions: 0, questions: []};
     this.quizzes.push(newQuiz);
     this.selectedQuiz = newQuiz;
   }
+
+  async saveChanges() {
+    try {
+      let result = await this.quizSvc.saveQuiz(true);
+      console.log(result);
+      } catch(error) {
+        console.log(error);
+      
+    }
+  }
+
+  cancelAllChanges() {
+
+    this.loadQuizzes();
+  }
+
+  addNewQuestion() {
+    let newQuestion = {name: "New Untitled Question"};
+    this.questions.push(newQuestion);
+
+  }
+
+  remove() {
+
+  }
+  
+
 }
