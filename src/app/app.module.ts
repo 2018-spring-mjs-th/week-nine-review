4cdf8132eb6b4032c54d8b7cf30ad6e3a1078d5f
tsteele <tsteele@madisoncollege.edu>
2018-04-15 18:42:55 -0500

    Initial CSS animation working...
    Need to make it work the Angular way ! ! !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -16,6 +17,7 @@ import { QuizService } from './quiz.service';
     BrowserModule
     , FormsModule
     , HttpModule
+    , BrowserAnimationsModule
   ],
   providers: [ QuizService ],
   bootstrap: [AppComponent]
