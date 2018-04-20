24f92611923e840b17b5f3035e8a155d2350ad16
ricabruinsma <tober@rocketmail.com>
2018-04-19 19:42:42 -0500

    new branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { QuizService } from './quiz.service';
 
 interface quizDisplay {
   name: string;
+  originalName: string;
   questions: questionDisplay[];
+  originalQuestionsString: string;
 }
 
 interface questionDisplay {
@@ -55,6 +57,11 @@ export class AppComponent {
       .then(data => {
         console.log("Promise fulfilled!!!");
         this.quizzes = data.json();
+        this.quizzes = this.quizzes.map(x=> ({ ...x
+          , originalName:x.name
+        , originalQuestionsString: x.questions.map(x => x.name).join("~")
+      }));
+        //this.quizzes = this.quizzes.map(x=> ({name: x.name, originalName: x.name, questions: x.questions}));
       })
       .catch(error => {
         console.log(error);
@@ -64,11 +71,13 @@ export class AppComponent {
   selectedQuiz: selectedQuizType = undefined;
 
   makeQuizSelected(q: quizDisplay) {
+    this.animateDetailsDisplay();
     this.selectedQuiz = q;
   }
 
   addQuiz() {
-    let newQuiz = { name: "New Untitled Quiz", questions: [] };
+    this.animateDetailsDisplay();
+    let newQuiz = { name: "New Untitled Quiz", originalName: "New Untitled Quiz", questions: [], originalQuestionsString: "" };
     this.quizzes.push(newQuiz);
     this.selectedQuiz = newQuiz;
   }
@@ -103,5 +112,22 @@ export class AppComponent {
     if (this.selectedQuiz) {
       this.selectedQuiz.questions.push({ name: "New Untitled Question" });
     }  
+  }
+
+  get numberOfEditedQuizzes() {
+    let editedQuizzes = this.quizzes.filter(x => 
+      x.name !== x.originalName
+      || x.originalName === "New Untitled Quiz"
+      || x.originalQuestionsString !== x.questions.map(x => x.name).join("~")
+    );
+
+    return editedQuizzes.length;
+  }
+
+  isDetailsDisplayAnimating = false;
+
+  private animateDetailsDisplay() {
+    this.isDetailsDisplayAnimating = true;
+    setTimeout(x => this.isDetailsDisplayAnimating = false, 500);
   }
 }
