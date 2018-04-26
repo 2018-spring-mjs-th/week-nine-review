5fd05b615d2b104a4201df87978662e1ecfce36b
HolliNiesen <holli.niesen@gmail.com>
2018-04-26 18:06:44 -0500

    Importing bootstrap module. Use forRoot() on root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { QuizService } from './quiz.service';
@@ -16,6 +16,7 @@ import { QuizService } from './quiz.service';
     BrowserModule
     , FormsModule
     , HttpModule
+    , NgbModule.forRoot()
   ],
   providers: [ QuizService ],
   bootstrap: [AppComponent]
