3a312616f05ec0777e37fe812019647ec829c148
emilysha <ewsha.09@gmail.com>
2018-04-26 18:02:34 -0500

    Added bootstrap

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -8,7 +8,7 @@ export class QuizService {
 
   public getQuizzes() {
 
-    return this.builtInAngularHttpService.get('https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Hard Quiz').toPromise();
+    return this.builtInAngularHttpService.get('https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name1=Hard Quiz').toPromise();
 
   }
 
